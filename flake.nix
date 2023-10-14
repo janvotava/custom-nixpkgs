7a1b35b042107f49f8c62533d0822929737e151d
Jan Votava <votava@deployment.cz>
2023-10-14 13:56:35 +0700

    refactor: remove unused code

--- flake.nix
+++ flake.nix
@@ -1,22 +1,11 @@
 {
   description = "Custom Nix packages";
 
-  outputs = { self, nixpkgs }:
+  outputs = { nixpkgs }:
     let
-      # pkgs = nixpkgs.legacyPackages.${system};
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
-      pkgs = forAllSystems (system: (nixpkgs.legacyPackages.${system}));
-      elasticsearch-8 = pkgs.callPackage ./elasticsearch/8.x.nix { };
     in
     {
-
       packages = forAllSystems (system: (nixpkgs.legacyPackages.${system}.callPackage ./default.nix { }));
-
-
-      # packages.x86_64-linux.elasticsearch-8 = elasticsearch-8;
-      # packages.x86_64-darwin.elasticsearch-8 = elasticsearch-8;
-      # packages.aarch64-linux.elasticsearch-8 = elasticsearch-8;
-      # packages.aarch64-darwin.elasticsearch-8 = elasticsearch-8;
-
     };
 }
